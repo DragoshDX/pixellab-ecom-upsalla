a7cee225be5d06462f5b2038694b187b2985d47f
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-01-18 21:19:33 +0200

    919

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Work_Sans } from 'next/font/google';
+import { css } from '@emotion/css';
 import './globals.css';
 
 const workSans = Work_Sans({ subsets: ['latin'] });
@@ -9,9 +10,27 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const gridCss = css`
+    display: grid;
+    grid-template-areas:
+      'header'
+      'main-area';
+    min-height: 100dvh;
+  `;
+
   return (
     <html lang="en">
-      <body className={workSans.className}>{children}</body>
+      <body className={workSans.className}>
+        <div className={gridCss}>
+          <header>header</header>
+
+          <div>
+            <main>{children}</main>
+
+            <footer>footer</footer>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
