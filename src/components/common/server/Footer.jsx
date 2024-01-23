68873d765f5d9d34a7950c50e60fcda8092f1889
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-01-23 21:52:37 +0200

    952

--- src/components/common/server/Footer.jsx
+++ src/components/common/server/Footer.jsx
@@ -1,3 +1,13 @@
+import { Separator } from '@/components/ui/server';
+
 export const Footer = () => {
-  return 'Footer Component';
+  return (
+    <>
+      <section className="container mx-auto px-4">footer primary</section>
+
+      <Separator></Separator>
+
+      <section className="container mx-auto px-4">footer secondary</section>
+    </>
+  );
 };
