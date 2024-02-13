1d6c1d78840ebba1f5366047e4c47cdf3e9ef079
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-02-13 21:07:15 +0200

    907

--- src/components/cart/client/AddToCart.jsx
+++ src/components/cart/client/AddToCart.jsx
@@ -1,11 +1,25 @@
+import { cartContext } from '@/contexts';
 import { useCartActions } from '@/hooks/cart';
+import { useContext } from 'react';
 
 export const AddToCart = ({ product }) => {
   const { title, id } = product;
   const { addToCart, adding, removeFromCart, removing } = useCartActions();
+  const { cartProducts } = useContext(cartContext);
+  let cartQuantity = 0;
+
+  const cartProduct = cartProducts.find(({ productId }) => {
+    return id === productId;
+  });
+
+  if (cartProduct) {
+    cartQuantity = cartProduct.quantity;
+  }
+
+  const isProductInCart = !!cartProduct;
 
   const onClick = () => {
-    addToCart(id);
+    isProductInCart ? removeFromCart(id, cartQuantity) : addToCart(id);
   };
 
   const busy = adding || removing;
@@ -15,10 +29,16 @@ export const AddToCart = ({ product }) => {
       type="button"
       className="bg-black text-white uppercase font-medium text-sm py-3 px-6 hover:bg-amber-800 transition-colors"
       disabled={busy}
-      title={`Add ${title} to cart`}
+      title={
+        isProductInCart ? `Remove ${title} from cart` : `Add ${title} to cart`
+      }
       onClick={onClick}
     >
-      {busy ? '...loading' : 'Add to Cart'}
+      {busy
+        ? '...loading'
+        : isProductInCart
+        ? 'Remove from Cart'
+        : 'Add to Cart'}
     </button>
   );
 };
